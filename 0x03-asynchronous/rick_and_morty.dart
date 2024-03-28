abb76ba2b3332ec41bdfebf283d683b67f86f6be
ydroal <droal.yoko@gmail.com>
2024-03-28 16:52:23 +0100

    update: add status check

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -5,13 +5,16 @@ Future<void> printRmCharacters() async {
   try {
     final url = Uri.https('rickandmortyapi.com', '/api/character');
     final response = await http.get(url);
-    final responseData = json.decode(response.body);
-    List<dynamic> characters = responseData['results'];
-
-    for (var character in characters) {
-      print(character['name']);
+    if (response.statusCode == 200) {
+      final responseData = json.decode(response.body);
+      List<dynamic> characters = responseData['results'];
+      for (var character in characters) {
+        print(character['name']);
+      }
+    } else {
+      print('Failed to fetch characters: ${response.statusCode}');
     }
   } catch (error) {
-    print('Error caught: $error');
+    print('error caught: $error');
   }
 }
